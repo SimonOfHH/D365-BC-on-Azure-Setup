42906007975b93f017f0097dff0763284d098bfd
Simon Fischer <sfischer6037@cosmoconsult.com>
2020-04-17 16:25:22 +0200

    Cleaned up file

--- D365BCOnAzureDeployment/Export/Private/Get-ApplicationGatewaySslSetupAndIdentity.ps1
+++ D365BCOnAzureDeployment/Export/Private/Get-ApplicationGatewaySslSetupAndIdentity.ps1
@@ -68,22 +68,5 @@ function Global:Get-ApplicationGatewaySslSetupAndIdentity {
             $SslSetup.Identity = $AppgwIdentity
         }
         $SslSetup
-        #xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-
-        Write-Verbose "Adding Frontend-Configuration..."
-        $FrontEndPorts = @{
-            PrivateHttp = $null
-            PublicHttp  = $null
-            PublicHttps = $null
-            Collection  = @()
-        }
-        $FrontEndPorts.PrivateHttp += New-AzApplicationGatewayFrontendPort -Name "FrontendPort_Private8080"  -Port 8080
-        $FrontEndPorts.PublicHttp += New-AzApplicationGatewayFrontendPort -Name "FrontendPort_Public80"  -Port 80        
-        $FrontEndPorts.Collection += ($FrontEndPorts.PrivateHttp,$FrontEndPorts.PublicHttp)
-        if ($IncludeSsl) {
-            $FrontEndPorts.PublicHttps += New-AzApplicationGatewayFrontendPort -Name "FrontendPort_Public443"  -Port 443            
-            $FrontEndPorts.Collection += ($FrontEndPorts.PublicHttps)
-        }        
-        $FrontEndPorts
     }
 }
