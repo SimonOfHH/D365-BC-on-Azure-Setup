ce5cce8bcbdab9eb2a2c59fc0c0c144880835772
Simon Fischer <sfischer6037@cosmoconsult.com>
2020-04-17 16:26:23 +0200

    Changed verbose message

--- D365BCOnAzureDeployment/Export/Private/Wait-ForVMShutdown.ps1
+++ D365BCOnAzureDeployment/Export/Private/Wait-ForVMShutdown.ps1
@@ -7,7 +7,7 @@ function Global:Wait-ForVMShutdown {
     process {        
         $VMShutdown = $false
         while (-not $VMShutdown) {
-            Write-Verbose "Sleeping for 5 seconds, waiting for VM to shutdown..."
+            Write-Verbose "Sleeping for 5 seconds, waiting for VM to shutdown $($VMName) $(Get-Date -Format "HH:mm:ss")..."
             Start-Sleep -Seconds 10  
             $statusCode = (Get-AzVM -ResourceGroupName $ResourceGroupName  -Name $VMName -Status).Statuses[1].Code
             $VMShutdown = ($statusCode -eq "PowerState/deallocated") -or ($statusCode -eq "PowerState/stopped")
