003ffdf931a54522bbf4f23884fad898fe3986b9
Simon Fischer <sfischer6037@cosmoconsult.com>
2020-04-17 16:23:18 +0200

    Added filtering on new parameter

--- D365BCOnAzureHelper/Export/Private/Get-CommandsFromStorageTable.ps1
+++ D365BCOnAzureHelper/Export/Private/Get-CommandsFromStorageTable.ps1
@@ -13,12 +13,18 @@ function Global:Get-CommandsFromStorageTable {
         [string]
         $TableName,
         [string]
-        $ObjectName
+        $ObjectName,
+        [Parameter(Mandatory = $false)]
+        [string]
+        $TypeFilter
     )
     process {
         $setupTable = Get-AzStorageTable -Name $TableName -Context $StorageAccountContext
         $setupCloudTable = $setupTable.CloudTable
-        $rows = Get-AzTableRow -Table $setupCloudTable | Where-Object { ($_.ObjectName -eq 'NULL') -or ($_.ObjectName -eq '') -or ($_.ObjectName -eq $ObjectName) } | Sort-Object -Property PartitionKey,RowKey
+        $rows = Get-AzTableRow -Table $setupCloudTable | Where-Object { ($_.ObjectName -eq 'NULL') -or ($_.ObjectName -eq '') -or ($_.ObjectName -eq $ObjectName) } | Sort-Object -Property PartitionKey, RowKey
+        if ($TypeFilter) {
+            $rows = $rows | Where-Object { ($_.TypeFilter -eq '') -or ($_.TypeFilter -eq $TypeFilter) } | Sort-Object -Property PartitionKey, RowKey
+        }
         $rows
     }
 }
