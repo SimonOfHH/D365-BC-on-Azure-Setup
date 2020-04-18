51d324b0fe3ab402d91def4cc1f0326e1cc4845f
Simon Fischer <sfischer6037@cosmoconsult.com>
2020-04-18 13:14:08 +0200

    Added alternative certificate handling

--- D365BCOnAzureDeployment/Export/Private/Get-ApplicationGatewaySslSetupAndIdentity.ps1
+++ D365BCOnAzureDeployment/Export/Private/Get-ApplicationGatewaySslSetupAndIdentity.ps1
@@ -26,13 +26,16 @@ function Global:Get-ApplicationGatewaySslSetupAndIdentity {
         $KeyVaultName,
         [Parameter(Mandatory = $false)]
         [string]
-        $CertificateName = "ApplicationGateway"
+        $CertificateName = "ApplicationGateway",
+        [Parameter(Mandatory = $false)]
+        [string]
+        $ApplicationGatewaySkuName
     )
     process {
         $SslSetup = @{
-            HandleSSL = $false
-            Certificate  = $null
-            Identity = $null
+            HandleSSL   = $false
+            Certificate = $null
+            Identity    = $null
         }
         
         if ($KeyVaultName) {
@@ -41,31 +44,47 @@ function Global:Get-ApplicationGatewaySslSetupAndIdentity {
                 $SslSetup.HandleSSL = $true
             }
         }
-        if (-not($SslSetup.HandleSSL)){
+        if (-not($SslSetup.HandleSSL)) {
             $SslSetup
             return 
         }
+        $certificateHandlingType = "V2"
+        if ($ApplicationGatewaySkuName) {
+            if (-not($ApplicationGatewaySkuName.Contains("v2"))) {
+                $certificateHandlingType = "V1"
+            }
+        }
         Write-Verbose "Preparing SSL Setup..."
         if ($SslSetup.HandleSSL) {
-            Write-Verbose "Retrieving certificate from KeyVault"
-            $secret = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $CertificateName
-            $secretId = $secret.Id.Replace($secret.Version, "") # https://<keyvaultname>.vault.azure.net/secrets/
-            $sslCertificate = New-AzApplicationGatewaySslCertificate -Name "$ApplicationGatewayName-certificate01" -KeyVaultSecretId $secretId
+            if ($certificateHandlingType -eq "V2") {
+                Write-Verbose "Retrieving certificate from KeyVault"
+                $secret = Get-AzKeyVaultSecret -VaultName $KeyVaultName -Name $CertificateName            
+                $secretId = $secret.Id.Replace($secret.Version, "") # https://<keyvaultname>.vault.azure.net/secrets/
+                $sslCertificate = New-AzApplicationGatewaySslCertificate -Name "$ApplicationGatewayName-certificate01" -KeyVaultSecretId $secretId
 
-            # Identity is needed, to be able to read from KeyVault
-            Write-Verbose "Generating identity for Application Gateway, to be able to read from KeyVault"
-            $Identity = Get-AzUserAssignedIdentity -Name "$ApplicationGatewayName-Identity01" -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
-            if (-not($Identity)) {
-                $Identity = New-AzUserAssignedIdentity -Name "$ApplicationGatewayName-Identity01" -ResourceGroupName $ResourceGroupName -Location $ResourceLocation
+                # Identity is needed, to be able to read from KeyVault
+                Write-Verbose "Generating identity for Application Gateway, to be able to read from KeyVault"
+                $Identity = Get-AzUserAssignedIdentity -Name "$ApplicationGatewayName-Identity01" -ResourceGroupName $ResourceGroupName -ErrorAction SilentlyContinue
+                if (-not($Identity)) {
+                    $Identity = New-AzUserAssignedIdentity -Name "$ApplicationGatewayName-Identity01" -ResourceGroupName $ResourceGroupName -Location $ResourceLocation
+                }
+                Wait-ForNewlyCreatedIdentity -ResourceGroupName $ResourceGroupName -ObjectId $Identity.PrincipalId  -Verbose:$Verbose
+
+                $AppgwIdentity = New-AzApplicationGatewayIdentity -UserAssignedIdentity $Identity.Id
+                Write-Verbose "Updating KeyVault-access policy for new identity"
+                Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ObjectId $Identity.PrincipalId -PermissionsToKeys get -PermissionsToSecrets get -PermissionsToCertificates get | Out-Null
+
+                $SslSetup.Certificate = $sslCertificate
+                $SslSetup.Identity = $AppgwIdentity
             }
-            Wait-ForNewlyCreatedIdentity -ResourceGroupName $ResourceGroupName -ObjectId $Identity.PrincipalId  -Verbose:$Verbose
-
-            $AppgwIdentity = New-AzApplicationGatewayIdentity -UserAssignedIdentity $Identity.Id
-            Write-Verbose "Updating KeyVault-access policy for new identity"
-            Set-AzKeyVaultAccessPolicy -VaultName $KeyVaultName -ResourceGroupName $ResourceGroupName -ObjectId $Identity.PrincipalId -PermissionsToKeys get -PermissionsToSecrets get -PermissionsToCertificates get | Out-Null
-
-            $SslSetup.Certificate = $sslCertificate
-            $SslSetup.Identity = $AppgwIdentity
+            else {
+                # The v1 Application Gateway doesn't support the direct linking to the KeyVault
+                # So we will download the certificate from the KeyVault and upload it as a new ApplicationGateway-certificate
+                $certificate = Get-AzKeyVaultCertificate -VaultName $KeyVaultName -Name $CertificateName
+                $certificateInfo = Save-AzureCertificateToLocalFile -KeyVaultName $KeyVaultName -Certificate $certificate -CertificateType $CertificateName -TargetFilename "$($env:TEMP)\$($CertificateName).pfx"
+                $sslCertificate = New-AzApplicationGatewaySslCertificate -Name "$ApplicationGatewayName-certificate01" -CertificateFile $certificateInfo.Path -Password (ConvertTo-SecureString $certificateInfo.Password -AsPlainText -Force)
+                $SslSetup.Certificate = $sslCertificate
+            }
         }
         $SslSetup
     }
