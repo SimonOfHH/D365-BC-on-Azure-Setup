221119f023c904e0100b8d916b2c2f646c0d8f78
Simon Fischer <sfischer6037@cosmoconsult.com>
2020-04-27 11:37:54 +0200

    Fixed error in Clear-ScaleSetPreparationResources

--- D365BCOnAzureDeployment/Export/Private/Clear-ScaleSetPreparationResources.ps1
+++ D365BCOnAzureDeployment/Export/Private/Clear-ScaleSetPreparationResources.ps1
@@ -19,9 +19,9 @@ function Global:Clear-ScaleSetPreparationResources {
         }
         Write-Verbose "Cleaning up resources..."
         Write-Verbose "Removing VM..."
-        Get-AzResource -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Compute/virtualMachines' | Where-Object {($_.Tags -ne $null) -and ((Compare-Hashtables $_.Tags $Tags) -eq $true)} | Remove-AzResource -Force | Out-Null
+        Get-AzResource -ResourceGroupName $ResourceGroupName -ResourceType 'Microsoft.Compute/virtualMachines' | Where-Object {($null -ne $_.Tags) -and ((Compare-Hashtables $_.Tags $Tags) -eq $true)} | Remove-AzResource -Force | Out-Null
         Write-Verbose "Removing child-resources..."
-        foreach ($resource in Get-AzResource -ResourceGroupName $ResourceGroupName | Where-Object {($_.Tags -ne $null) -and ((Compare-Hashtables $_.Tags $Tags) -eq $true)}) {
+        foreach ($resource in Get-AzResource -ResourceGroupName $ResourceGroupName | Where-Object {($null -ne $_.Tags) -and ((Compare-Hashtables $_.Tags $Tags) -eq $true) -and ($_.ResourceType -ne 'Microsoft.Compute/images')}) {
             Write-Verbose "Removing $($resource.Name)..."
             $resource | Remove-AzResource -Force  | Out-Null
         }       
